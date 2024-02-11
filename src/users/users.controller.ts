601f59d98020695163cfca641c42437ef967eb51
seniorsardorbek <sardorbekmusilman@gmail.com>
2024-02-11 15:38:19 +0500

    salom

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,23 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post
-} from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UsersService } from './users.service'
+import { IsLoggedIn } from 'src/auth/is-loggin.guard'
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor (private readonly usersService: UsersService) {}
   @Post()
-  
-  create(@Body() createUserDto: CreateUserDto) {
+  create (@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto)
   }
- 
+  @UseGuards(IsLoggedIn)
   @Get()
-  findAll() {
+  findAll () {
     return this.usersService.findAll()
   }
 
   @Get(':id')
-  findOne(@Param('id') id : string) {
+  findOne (@Param('id') id: string) {
     return this.usersService.findOne(id)
   }
-
 }
